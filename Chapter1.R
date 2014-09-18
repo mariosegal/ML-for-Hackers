9b09cb6c1a763d79af80934721a939ef52edcd35
Mario Segal <mario.segal@gmail.com>
2014-09-17 22:40:13 -0400

    9/17/2014

--- Chapter1.R
+++ Chapter1.R
@@ -7,8 +7,14 @@ x <- getURL("https://raw.githubusercontent.com/johnmyleswhite/ML_for_Hackers/mas
 ufo <- read.delim(text=x,sep="\t",stringsAsFactors=F,header=F,na.strings="")
 names(ufo) <- c("DateOcurred","DateReported","Location","ShortDescription","Duration","LongDescription")
 
-library(devtools)
-devtools::source_url("https://raw.github.com/johnmyleswhite/ML_for_Hackers/tree/master/01-Introduction/data/ufo/ufo_awesome.tsv")
 
-x <- ("https://raw.githubusercontent.com/johnmyleswhite/ML_for_Hackers/master/01-Introduction/data/ufo/ufo_awesome.tsv")
-ufo <- read.delim(text=x,sep="\t",stringsAsFactors=F,header=F,na.strings="")
+ufo$DateOcurred <- as.Date(ufo$DateOcurred,format="%Y%m%d")
+head(ufo[which(nchar(ufo$DateOcurred) !=8 | nchar(ufo$DateReported) != 8),1])
+
+good.rows <- ifelse(nchar(ufo$DateOcurred)!=8 | nchar(ufo$DateReported)!=8,F,T)
+table(good.rows)
+ufo <- ufo[good.rows]
+
+
+setwd("~/ML for Hackers")
+save.image()
